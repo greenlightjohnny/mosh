3ddb2d41dfaf11cad54747680c984894dd96e19c
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2020-02-03 08:24:36 -0500

    finished

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,29 +1,47 @@
 import React, { Component } from "react";
-
+import Counters from "./counters";
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: []
-  };
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+    }
+  }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>No renderTags</p>;
+  componentWillUnmount() {
+    console.log("Counter -umount");
+  }
+  render() {
+    console.log("Counter - Rendered");
     return (
-      <ul>
-        {this.state.tags.map(tag => {
-          return <li key={tag}>{tag}</li>;
-        })}
-      </ul>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={() => this.props.onIncremenent(this.props.counter)}
+        >
+          Increment
+        </button>
+        <button
+          id={this.props.counter.counterId}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.tags.length === 0 && "Please Create a new Tag"}
-        {this.renderTags()}
-      </React.Fragment>
-    );
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.props.counter;
+    return count === 0 ? "zero" : count;
   }
 }
 
